7cd95c171b829b770e29f958bc0427789086647d
Connor Mellencamp <comellen91@gmail.com>
2019-01-25 15:50:50 -0500

    initial commit. recreating berm in Angular 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { BikesComponent } from './bikes/bikes.component';
+import { TrailsComponent } from './trails/trails.component';
+import { RidesComponent } from './rides/rides.component';
+import { AuthComponent } from './auth/auth.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    BikesComponent,
+    TrailsComponent,
+    RidesComponent,
+    AuthComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/trails/trails.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trails',
+  templateUrl: './trails.component.html',
+  styleUrls: ['./trails.component.css']
+})
+export class TrailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
