c53f063a0dfe480d027d7b46b9069d0d78689a96
Connor Mellencamp <comellen91@gmail.com>
2019-02-13 04:31:59 -0500

    login form and component basically done

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -13,30 +13,42 @@ export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   submitted = false;
   returnUrl: string;
+  loading = false;
 
   constructor(
     private formBuilder: FormBuilder,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
     private authService: AuthService) { }
-
+  
   ngOnInit() {
     this.loginForm = this.formBuilder.group({
-      email: ['', Validators.required],
-      password: ['', Validators.required]
+      email: ['', Validators.required, Validators.email],
+      password: ['', Validators.required, Validators.minLength(6)]
     });
-    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    // this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+  }
+  
+  get f() { return this.loginForm.controls; }
+  
+  getEmailError() {
+    return this.f.email.hasError('required') ? 'Enter email' :
+      this.f.email.hasError('email') ? 'Enter valid email' :
+        '';
   }
 
-  get f() { return this.loginForm.controls; }
+  getPasswordError() {
+    return this.f.password.hasError('required') ? 'Enter password' :
+      this.f.password.hasError('minLength') ? 'Password must be at least 6 characters' :
+        '';
+  }
 
   onSubmit() {
     this.submitted = true;
     if (this.loginForm.invalid) {
       return;
     }
-    this.authService.login(this.f.email.value, this.)
+    this.loading = true;
+    this.authService.login(this.f.email.value, this.f.password.value)
   }
-
-
 }
