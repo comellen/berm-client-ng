f7246bc3c98c4a44b260a82471bbc9a219eac227
Connor Mellencamp <comellen91@gmail.com>
2019-02-19 19:46:12 -0500

    added canActivate function

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Router, CanActivate } from '@angular/router';
+import { AuthService } from '@app/services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuardService {
 
-  constructor() { }
+  constructor(public auth: AuthService, public router: Router) { }
+  canActivate(): boolean {
+    if (!this.auth.isAuthenticated()) {
+      this.router.navigate(['login']);
+      return false;
+    }
+    return true;
+  }
 }
