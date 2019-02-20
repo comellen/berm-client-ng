67c1aff8372df6d1e2449c3d00e1037b01b47d91
Connor Mellencamp <comellen91@gmail.com>
2019-02-19 19:15:21 -0500

    cleaned up auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,24 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
 
 const apiUrl = 'http://localhost:3000';
 
 @Injectable()
 export class AuthService {
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
-  login(email: string, password: string) {
-    return this.http.post<any>(apiUrl + '/user/login', { email, password })
-      .pipe(map(user => {
-        if (user && user.sessionToken) {
-          sessionStorage.setItem('currentUser', JSON.stringify(user));
-        }
-        return user;
-      }));
-  }
-
-  logout() {
-    sessionStorage.clear();
-  }
 }
