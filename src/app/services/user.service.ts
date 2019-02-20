ad030aba904e68e7a96a288b01ffaefe8f7edab1
Connor Mellencamp <comellen91@gmail.com>
2019-02-19 19:12:39 -0500

    moved login and logout functions to user service. added jwt

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 import { User } from '@app/models/user';
 
@@ -12,11 +13,25 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
+  login(email: string, password: string) {
+    return this.http.post<any>(apiUrl + '/user/login', { email, password })
+      .pipe(map(user => {
+        if (user && user.sessionToken) {
+          sessionStorage.setItem('currentUser', JSON.stringify(user));
+        }
+        return user;
+      }));
+  }
+
   signup(user: User) {
     return this.http.post(apiUrl + '/user/signup', user);
   }
 
   update(user: User) {
     return this.http.put(`/user/` + user.id, user);
+  }
+
+  logout() {
+    sessionStorage.clear();
   }
 }
