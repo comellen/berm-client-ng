d9b936747351991e4ec1bb0e70357d541d2b71f4
Connor Mellencamp <comellen91@gmail.com>
2019-03-19 15:44:36 -0400

    added getall functions in trail, bike, ride components

--- src/app/bikes/bikes.component.ts
+++ src/app/bikes/bikes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { AddBikeComponent } from '@app/bikes/add-bike/add-bike.component';
+import { BikeService } from '@app/services/bike.service';
+
 
 @Component({
   selector: 'app-bikes',
@@ -8,10 +10,24 @@ import { AddBikeComponent } from '@app/bikes/add-bike/add-bike.component';
   styleUrls: ['./bikes.component.scss']
 })
 export class BikesComponent implements OnInit {
+  user: any = sessionStorage.getItem('user');
+  bikes: any = [];
 
-  constructor(private dialog: MatDialog) { }
+  constructor(
+    private dialog: MatDialog,
+    private bikeService: BikeService,
+    ) { }
 
   ngOnInit() {
+    this.getBikes();
+  }
+
+  getBikes() {
+    this.bikes = [];
+    this.bikeService.getBikes(this.user)
+      .subscribe((data: any) => {
+        this.bikes = data;
+      });
   }
 
   openDialog() {
--- src/app/rides/rides.component.ts
+++ src/app/rides/rides.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { AddRideComponent } from '@app/rides/add-ride/add-ride.component';
+import { RideService } from '@app/services/ride.service';
 
 @Component({
   selector: 'app-rides',
@@ -8,10 +9,24 @@ import { AddRideComponent } from '@app/rides/add-ride/add-ride.component';
   styleUrls: ['./rides.component.scss']
 })
 export class RidesComponent implements OnInit {
+  user: any = sessionStorage.getItem('user');
+  rides: any = [];
 
-  constructor(private dialog: MatDialog) { }
+  constructor(
+    private dialog: MatDialog,
+    private rideService: RideService,
+    ) { }
 
   ngOnInit() {
+    this.getRides();
+  }
+
+  getRides() {
+    this.rides = [];
+    this.rideService.getRides(this.user)
+      .subscribe((data: any) => {
+        this.rides = data;
+      });
   }
 
   openDialog() {
