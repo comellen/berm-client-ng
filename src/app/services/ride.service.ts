d8fb0f6fc2df91a7c2a804baec1aaac888f4f504
Connor Mellencamp <comellen91@gmail.com>
2019-03-19 15:35:09 -0400

    added getall functions to bike, trail, and ride services.

--- src/app/services/ride.service.ts
+++ src/app/services/ride.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Ride } from '@app/models/ride';
+import { User } from '@app/models/user';
+
+const apiUrl = 'http://localhost:3000';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RideService {
+  user: any = sessionStorage.getItem('user');
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getRides(user: any) {
+    return this.http.get<Ride[]>(apiUrl + '/rides/getall', this.user);
+  }
 }
